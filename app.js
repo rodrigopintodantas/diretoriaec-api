67b2d2dc48081a0f8e0d939fd40da410ecfe745e
Rodrigo Pinto Dantas <rodrigo.pdantas@tjdft.jus.br>
2026-04-22 07:12:15 -0300

    fix: atualização

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
@@ -8,6 +9,8 @@ const indexRouter = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
 
 const app = express();
+const publicDir = path.join(__dirname, "public");
+const spaIndexPath = path.join(publicDir, "index.html");
 
 app.use(
   cors({
@@ -21,9 +24,16 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(publicDir));
 
 app.use("/api", indexRouter);
+
+app.get(/^\/(?!api).*/, (req, res, next) => {
+  if (path.extname(req.path)) return next();
+  if (!fs.existsSync(spaIndexPath)) return next();
+  return res.sendFile(spaIndexPath);
+});
+
 app.use(errorHandler);
 
 module.exports = app;
